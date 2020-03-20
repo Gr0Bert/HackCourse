07b8979a258ea7aa8c026f87d595b3f4b0306698
Rob <gubinrobert@gmail.com>
2020-03-20 21:47:12 +0100

    hardware definitions added

---
+++ hardware/04/fill/Fill.asm
@@ -0,0 +1,80 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Put your code here.
+
+(LOOP)
+@KBD
+D=M
+
+@PRESSED
+D;JGT
+
+@FREED
+D;JEQ
+
+@LOOP
+0;JMP
+
+
+(PRESSED)
+// color = -1
+@color
+M=-1
+@FILL
+0;JMP
+
+
+(FREED)
+//color = 0
+@color
+M=0
+@FILL
+0;JMP
+
+(FILL)
+//i = 8192
+@8192
+D=A
+@i
+M=D
+@FILL_LOOP
+0;JMP
+
+(FILL_LOOP)
+
+// addressToPaint = SCREEN + i
+@SCREEN
+D=A
+@i
+D=D+M
+@addressToPaint
+M=D
+
+@color
+D=M
+@addressToPaint
+A=M
+
+M=D // paint
+
+@i
+M=M-1
+D=M
+
+@FILL_LOOP
+D;JGT
+
+@LOOP
+0;JMP
+
