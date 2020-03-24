6e6a4eae5c707abbdfa13447b235730dc142df55
Rob <gubinrobert@gmail.com>
2020-03-25 00:08:08 +0100

    Comments and empty lines support

--- src/main/scala/assembler/Parser.scala
+++ src/main/scala/assembler/Parser.scala
@@ -1,9 +1,10 @@
 package assembler
 
 import fastparse._
-import NoWhitespace._
 
 object Parser {
+  import NoWhitespace._
+
   sealed trait Expression
   sealed trait AddressInstruction extends Expression
   final case class Constant(value: Int) extends AddressInstruction
@@ -19,7 +20,7 @@ object Parser {
     value.toIntOption.map(Constant).getOrElse(Reference(value))
   }.log
 
-  private def dest[_: P] = P(CharIn("ADM").rep.! ~ "=").log
+  private def dest[_: P] = P(("A" | "D" | "M").! ~ "=").log
   // I don't understand why it is not working without `!"("
   // somehow `comp` parser succeeds
   private def comp[_: P] = P(!"(" ~ (CharIn("ADM", "10+!&|") | "-").rep.!).log
@@ -29,17 +30,25 @@ object Parser {
       case (dest, comp, jmp) => CInstruction(dest, comp, jmp)
     }.log
   private def label[_: P] = P("(" ~ (alpha | underscoreOrDash).rep.! ~ ")").map(Label).log
-  private def lineSeparator[_: P] = P(System.lineSeparator())
-  private def expr[_: P] = P(aInstruction | cInstruction | label).log
+  private def lineSeparator[_: P] = P(System.lineSeparator()).log
+  private def tabOrSpace[_: P] = P(" " | "\t").log
+  private def comment[_: P] = P( ("//" ~ CharsWhile(c => (c != '\r' && c != '\n')).rep)).log
+  private def expr[_: P] = P( (aInstruction | cInstruction | label) ~ tabOrSpace.rep ~ comment.?).log
   // here separator should  be done only with `....rep(sep = lineSeparator)`
   // direct adding on `~ (lineSeparator | End)` doesn't work
-  private def exprList[_: P] = P(expr.rep(sep = lineSeparator) ~ End)
+  private def exprList[_: P] = P(expr.rep(sep = lineSeparator) ~ End).log
   private def onFailure(x: String, y: Int, z: Parsed.Extra) = Left((x, y, z))
   private def onSuccess(exp: Seq[Expression], index: Int) = Right((index, exp))
 
   type FailureString = String
   type Index = Int
   def parseRaw(raw: String): Either[(FailureString, Index, Parsed.Extra), (Index, Seq[Expression])] = {
-    parse(raw, exprList(_)).fold(onFailure, onSuccess)
+    val normalisedString =
+      raw
+        .split(System.lineSeparator())
+        .map(str => str.replace("\n", "").replace("\r", "").strip)
+        .filterNot(str => str.isEmpty || str.startsWith("//") || str.isBlank)
+    val normalised = normalisedString.mkString(System.lineSeparator())
+    parse(normalised, exprList(_)).fold(onFailure, onSuccess)
   }
 }
