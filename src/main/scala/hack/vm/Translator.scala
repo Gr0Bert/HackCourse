6c724ce5456b9b120f4214f47cabf098cfd74408
Rob <gubinrobert@gmail.com>
2020-04-13 15:06:09 +0200

    Memory access, addition/substraction

--- src/main/scala/hack/vm/Translator.scala
+++ src/main/scala/hack/vm/Translator.scala
@@ -1,10 +1,20 @@
 package hack.vm
 
-import hack.vm.StackMachine.Arithmetic.Add
-import hack.vm.StackMachine.{Command, MemoryAccess}
+import hack.vm.StackMachine.{Arithmetic, Command, MemoryAccess}
 import hack.vm.StackMachine.MemoryAccess.{Addressable, Segment}
 
 object Translator extends App {
+  sealed trait Register
+  case object A extends Register
+  case object M extends Register
+  case object D extends Register
+
+  implicit def regToStr(r:Register): String = r.toString
+
+  implicit def regToAsm(r: Register): Option[String] = {
+    Some(r.toString)
+  }
+
   sealed trait MemoryAccessType
   final case class Direct(offset: Int) extends MemoryAccessType
   final case class Indirect(label: String) extends MemoryAccessType
@@ -21,9 +31,35 @@ object Translator extends App {
   }
 
   import hack.assembler.Assembly._
+
+  def performBinaryArithmetic(instruction: CInstruction) = {
+    List(
+      Reference("SP"),
+      CInstruction(M, "M-1", None),
+      CInstruction(A, M, None),
+      CInstruction(D, M, None),
+      Reference("SP"),
+      CInstruction(M, "M-1", None),
+      CInstruction(A, M, None),
+      instruction,
+      Reference("SP"),
+      CInstruction(M, "M+1", None)
+    )
+  }
+
   def eval(command: Command, filename: String): List[Expression] = {
     command match {
-      case _: StackMachine.Arithmetic => ???
+      case arithmetic: StackMachine.Arithmetic => arithmetic match {
+        case Arithmetic.Add => performBinaryArithmetic(CInstruction(M, s"D+M", None))
+        case Arithmetic.Sub => performBinaryArithmetic(CInstruction(M, "M-D", None))
+        case Arithmetic.Neg => ???
+        case Arithmetic.Eq => ???
+        case Arithmetic.Gt => ???
+        case Arithmetic.Lt => ???
+        case Arithmetic.And => performBinaryArithmetic(CInstruction(M, "D&M", None))
+        case Arithmetic.Or => performBinaryArithmetic(CInstruction(M, "D|M", None))
+        case Arithmetic.Not => ???
+      }
       case access: StackMachine.MemoryAccess => access match {
         case MemoryAccess.Pop(segment, address) => segment match {
           case segment: MemoryAccess.Special => segment match {
@@ -31,35 +67,43 @@ object Translator extends App {
             case Segment.Static =>
               List(
                 Reference("SP"),
-                CInstruction(Some("A"), "M", None),
-                CInstruction(Some("D"), "M", None),
+                CInstruction(M, "M-1", None),
+                CInstruction(A, M, None),
+                CInstruction(D, M, None),
                 Reference(s"$filename.$address"),
-                CInstruction(Some("M"), "D", None),
+                CInstruction(M, D, None),
                 Reference("SP"),
-                CInstruction(Some("M"), "M-1", None)
               )
           }
           case segment: MemoryAccess.Addressable => offset(segment) match {
             case Direct(offset) =>
               List(
                 Reference("SP"),
-                CInstruction(Some("A"), "M", None),
-                CInstruction(Some("D"), "M", None),
+                CInstruction(M, "M-1", None),
+                CInstruction(A, M, None),
+                CInstruction(D, M, None),
                 Constant(offset + address),
-                CInstruction(Some("M"), "D", None),
+                CInstruction(M, D, None),
                 Reference("SP"),
-                CInstruction(Some("M"), "M-1", None)
               )
             case Indirect(label) =>
               List(
-                Reference("SP"),
-                CInstruction(Some("A"), "M", None),
-                CInstruction(Some("D"), "M", None),
                 Reference(label),
-                CInstruction(Some("A"), "M", None),
-                CInstruction(Some("M"), "D", None),
+                CInstruction(D, M, None),
+                Constant(address),
+                CInstruction(D, s"D+A", None),
+
+                Reference("R13"),
+                CInstruction(M, D, None),
+
                 Reference("SP"),
-                CInstruction(Some("M"), "M-1", None)
+                CInstruction(M, "M-1", None),
+                CInstruction(A, M, None),
+                CInstruction(D, M, None),
+
+                Reference("R13"),
+                CInstruction(A, M, None),
+                CInstruction(M, D, None),
               )
           }
         }
@@ -68,22 +112,22 @@ object Translator extends App {
             case Segment.Constant =>
               List(
                 Constant(address),
-                CInstruction(Some("D"), "A", None),
+                CInstruction(D, "A", None),
                 Reference("SP"),
-                CInstruction(Some("A"), "M", None),
-                CInstruction(Some("M"), "D", None),
+                CInstruction(A, "M", None),
+                CInstruction(M, "D", None),
                 Reference("SP"),
-                CInstruction(Some("M"), "M+1", None)
+                CInstruction(M, "M+1", None)
               )
             case Segment.Static =>
               List(
                 Reference(s"$filename.$address"),
-                CInstruction(Some("D"), "A", None),
+                CInstruction(D, "A", None),
                 Reference("SP"),
-                CInstruction(Some("A"), "M", None),
-                CInstruction(Some("M"), "D", None),
+                CInstruction(A, "M", None),
+                CInstruction(M, "D", None),
                 Reference("SP"),
-                CInstruction(Some("M"), "M+1", None)
+                CInstruction(M, "M+1", None)
               )
           }
           case addressable: Addressable =>  offset(addressable) match {
@@ -92,21 +136,25 @@ object Translator extends App {
                 Constant(offset + address),
                 CInstruction(Some("D"), "M", None),
                 Reference("SP"),
-                CInstruction(Some("A"), "M", None),
-                CInstruction(Some("M"), "D", None),
+                CInstruction(A, "M", None),
+                CInstruction(M, "D", None),
                 Reference("SP"),
-                CInstruction(Some("M"), "M+1", None)
+                CInstruction(M, "M+1", None)
               )
             case Indirect(label) =>
               List(
                 Reference(label),
-                CInstruction(Some("A"), "M", None),
-                CInstruction(Some("D"), "M", None),
+                CInstruction(D, M, None),
+                Constant(address),
+                CInstruction(D, s"D+A", None),
+                CInstruction(A, D, None),
+                CInstruction(D, M, None),
+
                 Reference("SP"),
-                CInstruction(Some("A"), "M", None),
-                CInstruction(Some("M"), "D", None),
+                CInstruction(A, M, None),
+                CInstruction(M, D, None),
                 Reference("SP"),
-                CInstruction(Some("M"), "M+1", None)
+                CInstruction(M, "M+1", None)
               )
           }
         }
@@ -144,6 +192,11 @@ object Translator extends App {
     Push(Segment.Temp, 6),
     Add,
   )
-  val res = p.flatMap(eval(_, "Main.vm"))
-  println(res.mkString(System.lineSeparator()))
+  p.foreach{ command =>
+    println{
+      s"""// $command
+         |${eval(command, "Main.vm").mkString(System.lineSeparator())}
+         |""".stripMargin
+    }
+  }
 }
